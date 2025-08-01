9fe111662ae3be6b6253a89a008de9a7bd906cf1
vizsim <s.metzler@mailbox.org>
2025-08-01 10:53:58 +0200

    got new ml traffic sign data, updated sc7 accordingly

--- js/mapdata/addSources.js
+++ js/mapdata/addSources.js
@@ -52,7 +52,9 @@ export function addSources(map, { MAPTILER_API_KEY, MAPILLARY_TOKEN }) {
   // addPMTilesSource("scenario7", "scenario7_missing-cw_bbb.pmtiles");
   //addPMTilesSource("scenario7", "scenarios/scenario7_trafficsigns_missing-cw_osm250528_ml250607.pmtiles");
   //addPMTilesSource("scenario7", "scenarios/scenario7_trafficsigns_missing-cw_osm250721_ml250607.pmtiles");
-  addPMTilesSource("scenario7", "scenarios/scenario7_trafficsigns_missing-cw_osm250721_ml250607_ml1y25_.pmtiles");
+  //addPMTilesSource("scenario7", "scenarios/scenario7_trafficsigns_missing-cw_osm250721_ml250607_ml1y25_.pmtiles");
+  addPMTilesSource("scenario7", "scenarios/scenario7_trafficsigns_missing-cw_osm250731_ml250730_ml1y25.pmtiles");
+  
 
 
 
